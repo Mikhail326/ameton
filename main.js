6d98276932149ec152e6efb60702bd3955d663c1
Mikhail326 <misha.stelmakh1995@gmail.com>
2024-01-12 10:10:44 +0300

    finish

--- main.js
+++ main.js
@@ -1,15 +1,33 @@
-const referencesItems = document.querySelectorAll('.reference__item .item__title')
+const referencesItems = document.querySelectorAll(".reference__item .item__title");
+const slider = document.querySelector(".questions__list");
+const sliderArrows = document.querySelectorAll(".slider-arrow");
+const sliderFirstItem = slider.querySelectorAll(".questions__item")[0];
 
 const accordionToggle = (e) => {
-  e.currentTarget.classList.toggle('active')
-  const item = e.currentTarget.nextElementSibling
-  if (e.currentTarget.classList.contains('active')) {
-    item.style.maxHeight = item.scrollHeight + 'px'
+  e.currentTarget.classList.toggle("active");
+  const item = e.currentTarget.nextElementSibling;
+  if (e.currentTarget.classList.contains("active")) {
+    item.style.maxHeight = item.scrollHeight + "px";
   } else {
-    item.style.maxHeight = 0
+    item.style.maxHeight = 0;
   }
-}
+};
 
-referencesItems.forEach(r => {
-  r.addEventListener('click', accordionToggle)
-})
+const showHideArrow = () => {
+  let scrollWidth = slider.scrollWidth - slider.clientWidth;
+  sliderArrows[0].style.display = slider.scrollLeft == 0 ? "none" : "block";
+  sliderArrows[1].style.display = slider.scrollLeft == scrollWidth ? "none" : "block";
+};
+
+referencesItems.forEach((r) => {
+  r.addEventListener("click", accordionToggle);
+});
+
+sliderArrows.forEach((a) => {
+  a.addEventListener("click", () => {
+    let sliderFirstItemWidth = sliderFirstItem.clientWidth + 24;
+    slider.scrollLeft +=
+      a.id == "left" ? -sliderFirstItemWidth : sliderFirstItemWidth;
+    showHideArrow();
+  });
+});
